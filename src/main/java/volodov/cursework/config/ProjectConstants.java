0c63ba5d389ff0e1878749769e0b685c004982d2
mikhailkatarzhin <maikl.1997@mail.ru>
2022-12-17 13:36:42 +0400

    добавлены конфиги для спринга

---
+++ src/main/java/volodov/cursework/config/ProjectConstants.java
@@ -0,0 +1,5 @@
+package volodov.cursework.config;
+
+public class ProjectConstants {
+    public static final Long ROW_COUNT = 10L;
+}
---
+++ src/main/java/volodov/cursework/config/WebSecurityConfig.java
@@ -0,0 +1,70 @@
+package volodov.cursework.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public static BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public WebSecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/administration/**")
+                        .hasAnyAuthority("АДМИНИСТРАТОР")
+                    .antMatchers("/consumer/**")
+                        .hasAnyAuthority("ПОТРЕБИТЕЛЬ")
+                    .antMatchers("/driver/**")
+                        .hasAnyAuthority("ВОДИТЕЛЬ")
+                    .antMatchers("/sign_in", "/sign_up")
+                        .anonymous()
+                    .antMatchers("/profile/**")
+                        .authenticated()
+                    .antMatchers("/", "/webjars/**", "/css/**", "/home")
+                        .permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                    .loginPage("/sign_in")
+                    .defaultSuccessUrl("/profile")
+                    .permitAll()
+                .and()
+                    .logout()
+                    .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(encoder())
+                .usersByUsernameQuery("select username, password, 'true' from \"User\" where username=?")
+                .authoritiesByUsernameQuery("\n" +
+                        "select u.username, r.name from \"user\" u" +
+                        " inner join role r on u.role_id = r.id" +
+                        " where u.username=?");
+    }
+}
